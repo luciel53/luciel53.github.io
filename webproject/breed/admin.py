a70f0d55e9ad17ef7abf7dabe517258ca0c31147
luciel53 <l.leroty@gmail.com>
2023-07-27 12:27:57 +0200

    deploy website

--- webproject/breed/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import annimal_post
-
-admin.site.register(annimal_post)
-# Register your models here.
--- webproject/breed/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class BreedConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'breed'
