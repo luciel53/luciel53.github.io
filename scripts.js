fbb6cfaafafeda8251b8b6da559d00fd1767b2a2
luciel53 <l.leroty@gmail.com>
2024-01-06 12:32:51 +0100

    changes

--- scripts.js
+++ scripts.js
@@ -29,4 +29,25 @@ document.addEventListener('DOMContentLoaded', function () {
 	// Écouteurs d'événements pour les boutons de contrôle
 	prevButton.addEventListener('click', showPrevItem);
 	nextButton.addEventListener('click', showNextItem);
+
+
+	/* top button */
+	var toTopButton = document.getElementById("to-top-button");
+
+	// When the user scrolls down 200px from the top of the document, show the button
+	window.onscroll = function () {
+	    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+	        toTopButton.classList.remove("hidden");
+	    } else {
+	        toTopButton.classList.add("hidden");
+	    }
+	}
+	// When the user clicks on the button, scroll to the top of the document
+	function goToTop() {
+	    window.scrollTo({ top: 0, behavior: 'smooth' });
+	}
+	toTopButton.addEventListener('click', goToTop);
+
+
+
 });
