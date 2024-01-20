9d0d70876da468864a2777441511818dfc7cb0c1
luciel53 <l.leroty@gmail.com>
2024-01-20 13:15:20 +0100

    update headphones banner

--- scripts.js
+++ scripts.js
@@ -1,35 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
-	const carousel = document.querySelector('[data-carousel="static"]');
-	const prevButton = document.querySelector('[data-carousel-prev]');
-	const nextButton = document.querySelector('[data-carousel-next]');
-	const items = document.querySelectorAll('[data-carousel-item]');
-	let currentIndex = 0;
-
-	// Function to print next element in the carousel
-	function showNextItem() {
-		items[currentIndex].classList.remove('duration-700', 'ease-in-out', 'hidden');
-		currentIndex = (currentIndex + 1) % items.length;
-		items[currentIndex].classList.add('duration-700', 'ease-in-out');
-		items[currentIndex].classList.remove('hidden');
-	}
-
-	// Function to print previous element in the carousel
-    function showPrevItem() {
-        // Update currentIndex before showing the previous item
-        currentIndex--;
-        if (currentIndex < 0) {
-            currentIndex += items.length;
-        }
-
-        items[currentIndex].classList.remove('duration-700', 'ease-in-out', 'hidden');
-        items[currentIndex].classList.add('duration-700', 'ease-in-out');
-        items[currentIndex].classList.remove('hidden');
-    }
-
-	// Écouteurs d'événements pour les boutons de contrôle
-	prevButton.addEventListener('click', showPrevItem);
-	nextButton.addEventListener('click', showNextItem);
-
 
 	/* top button */
 	var toTopButton = document.getElementById("to-top-button");
